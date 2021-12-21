9860875625c531d051345cd9cd037caf37c183ab
Flavio deSouza <flavio.desouza@alphasights.com>
2021-12-21 14:53:36 -0300

    rails: controllers and views

--- rails/simple_cms/app/controllers/demo_controller.rb
+++ rails/simple_cms/app/controllers/demo_controller.rb
@@ -3,5 +3,12 @@ class DemoController < ApplicationController
   layout false
 
   def index
+    @id = params[:id]
+    @page = params[:page]
+  end
+
+  def hello
+    @array = [1,2,3,4,5]
+    #redirect_to(controller: 'demo', action: 'index')
   end
 end
