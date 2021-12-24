6e45c4ac2bfb5266bb0a2c5cf5e094a699b718f9
Flavio deSouza <flavio.desouza@alphasights.com>
2021-12-24 09:03:24 -0300

    CRUD

---
+++ rails/blueprint/config/routes.rb
@@ -0,0 +1,18 @@
+Rails.application.routes.draw do
+
+  resources :subjects
+  resources :pages
+
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
+
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
