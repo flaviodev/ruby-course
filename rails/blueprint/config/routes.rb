2e5867b54f00bfced9a43f5b72105d7e318df303
Flavio deSouza <flavio.desouza@alphasights.com>
2021-12-27 13:30:44 -0300

    CRUD

--- rails/blueprint/config/routes.rb
+++ rails/blueprint/config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get 'home/index'
   resources :subjects do
     member do
       get :delete
@@ -15,16 +14,23 @@ Rails.application.routes.draw do
     end
   end
 
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
+  #
   # get 'subjects/index'
   # get 'subjects/show'
   # get 'subjects/new'
   # get 'subjects/edit'
   # get 'subjects/delete'
 
-  # get 'pages/index'
-  # get 'pages/show'
-  # get 'pages/new'
-  # get 'pages/edit'
-  # get 'pages/delete'
+  #default route
+  #get ':controller(/:action(/:id))'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
 end
